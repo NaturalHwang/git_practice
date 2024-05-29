691d797c8ae286c964d6e2c3f6b91dbfaa15f5bf
NaturalHwang <junghahwang0601@gmail.com>
2024-05-29 16:09:51 +0900

    fix

--- basic_syntax/2.basic_command.sh
+++ basic_syntax/2.basic_command.sh
@@ -38,4 +38,15 @@ git checkout 커밋ID
 git checkout main
 
 # git pull은 원격의 변경 사항을 local로 다운로드
-git pull 
+git pull origin 브랜치명
+
+# git 작업 취소
+# working directory에서 취소
+git checkout .
+# staging area에서 취소
+git reset
+# commit 이후의 취소
+git reset HEAD~1
+git reset HEAD^
+# push 이후의 취소
+git revert 커밋ID
